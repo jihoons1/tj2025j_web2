9ce45f02d337d9363b58f7d433ffab1bda053a73
songjihoons1210 <gnsthd0621@naver.com>
2025-09-01 12:24:13 +0900

    3교시 : [ 스프링 스케줄링 ] @Scheduled = 밀리초 사용방법

--- src/main/java/org/example/day01/_1스프링스레드/AppStart.java
+++ src/main/java/org/example/day01/_1스프링스레드/AppStart.java
@@ -1,4 +1,4 @@
-package org.example.day01;
+package org.example.day01._1스프링스레드;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
