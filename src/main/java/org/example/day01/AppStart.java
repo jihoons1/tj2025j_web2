3b5e1e6438c5fe88a762f645057c9be6ac5242ce
songjihoons1210 <gnsthd0621@naver.com>
2025-09-01 10:26:18 +0900

    1교시 : [Thread 사용법] web 지연시간 걸기 동기,비동기 활용 AppStart.java : @EnableAsync // 비동기 사용 활성화

--- src/main/java/org/example/day01/AppStart.java
+++ src/main/java/org/example/day01/AppStart.java
@@ -1,5 +1,14 @@
 package org.example.day01;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync // 비동기 사용 활성화
 public class AppStart {
-    //
-}
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+
+    } // main end
+} // class end
