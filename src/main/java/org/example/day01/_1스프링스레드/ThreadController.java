28d46a0abbc83eaa348e933d226e14d0ed3dc196
songjihoons1210 <gnsthd0621@naver.com>
2025-09-01 11:38:52 +0900

    2교시 : [ 멀티 Thread 사용법] web 지연시간 걸기 동기,비동기 활용

---
+++ src/main/java/org/example/day01/_1스프링스레드/ThreadController.java
@@ -0,0 +1,32 @@
+package org.example.day01._1스프링스레드;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController                         // 1. http 요청/응답 처리
+@RequestMapping("/task/day01")          // 2. http url 연결(메핑)
+@RequiredArgsConstructor                // 3. final 변수의 생성자
+public class ThreadController {
+    // [*] 서비스 객체
+    private final ThreadService threadService;
+
+    // 1.
+    @GetMapping // 동기화 : 스프링 메핑 컨트롤러는 자동 동기화 처리한다.
+    // 먼저 요청한 http 부터 처리후 반환 한다. // 순서대로
+    // http://localhost:8080/task/day01
+    public int thread1(){
+        System.out.println("ThreadController.thread");
+        return threadService.thread1();
+    }
+
+    // 2.
+    @DeleteMapping // 비동기화 : 스프링 메핑 컨트
+    // http://localhost:8080/task/day01
+    public void thread2(){
+        System.out.println("ThreadController.thread2");
+//        return threadService.thread2();
+    }
+}
---
+++ src/main/java/org/example/day01/_1스프링스레드/ThreadService.java
@@ -0,0 +1,38 @@
+package org.example.day01._1스프링스레드;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ThreadService {
+
+    // 1.
+    public int thread1(){
+        int result = 0;
+        // 반복문
+        for ( int i = 1 ; i<=10 ; i++ ){
+            result += i; // result 에 i+ 증가 , 누적
+            System.out.println("ThreadController.thread\n"+result);
+            // 만약에 서비스 처리가 늦어진다면 HTTP의 반환?? **
+            try{ Thread.sleep(1000); // 현재 스레드 1초간 일시정지 , * Talenn Send 시 HTTP 응답 10초 후 다시 가능
+            }catch (Exception e) { System.out.println(e); } // 예외 처리
+        } // for end
+        // 위 누적값 반환
+        return result;
+    }// func end
+
+    // 2.
+    @Async // 응답을 먼저하고 내부적으로 처리하는 상황
+    public void thread2(){
+        int result = 0;
+        // 반복문
+        for (int i = 1; i<=10 ; i++){
+            result += i;
+            System.out.println("ThreadController.thread2");
+            try{ Thread.sleep(1000); }
+            catch (Exception e) { System.out.println( e ); }
+        }
+//        return result;
+    } // func end
+
+} // class end
