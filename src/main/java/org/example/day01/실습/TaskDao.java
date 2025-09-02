8d23e786d38cd1de6f63197e0bfa46a96c950ca1
songjihoons1210 <gnsthd0621@naver.com>
2025-09-02 10:29:25 +0900

    1교시 : [ websocket ] 핸들러 경로, 주소

---
+++ src/main/java/org/example/day01/실습/TaskDao.java
@@ -0,0 +1,74 @@
+package org.example.day01.실습;
+
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Repository
+public class TaskDao {
+
+    private String u_sql = "jdbc:mysql://localhost:3306/springweb2";
+    private String u_name = "root";
+    private String u_pass = "1234";
+    public Connection conn;
+
+    public TaskDao() { connect(); }
+
+    private void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(u_sql, u_name, u_pass);
+            System.out.println("TaskDao.connect");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    // 30초
+    public int task11() {
+        try{
+            String sql = "update products set stock_quantity = stock_quantity - 3 ";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            int asd = ps.executeUpdate();
+            return asd;
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return 0;
+    }
+
+    // 1분
+    public String task22() {
+        try{
+            String sql = "select * from products ";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                String product_name = rs.getString("product_name");
+                int stock_quantity = rs.getInt("stock_quantity");
+                System.out.println(product_name + "\n" + stock_quantity);
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    // 5분
+    public int task33() {
+        try{
+            String sql = "update products set stock_quantity = stock_quantity + 20 where stock_quantity <= 10 ";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            int rs = ps.executeUpdate();
+            return rs;
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return 0;
+    }
+
+
+}
---
+++ src/main/java/org/example/day01/실습/TaskService.java
@@ -0,0 +1,37 @@
+package org.example.day01.실습;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    @Autowired
+    TaskDao taskDao;
+
+    // 1. 매30초마다 모든 제품의 재고는 3개씩 감소
+    @Scheduled(cron = "*/5 * * * * *")
+    public void task1() {
+        System.out.println("[30초]TaskService.task");
+        int asd = taskDao.task11();
+        // ++Dao 호출하여 모든 제품3개씩 감소 요청/응답++
+    }
+
+    // 2. 매 1분마다 모든 제품 정보를 조회/출력 한다.
+    @Scheduled(cron = "*/15 * * * * *") // 1분마다
+    public void task2() {
+        System.out.println("[1분]TaskService.task");
+        String asd = taskDao.task22();
+        // ++ 모든 제품 정보를 조회/출력한다. ++
+    }
+
+    // 3. 매 5분마다 재고가 10 이하인 상품의 재고를 +20개 추가한다.
+    @Scheduled(cron = "*/15 * * * * *") // 5분마다
+    public void task3() {
+        System.out.println("[5분]TaskService.task3");
+        int asd = taskDao.task33();
+        // ++ 재고가 10 이하인 상품의 재고를 +20개 추가한다.++
+    }
+
+
+}
